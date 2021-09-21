d11e1fca0b63f73af349e397765f879dd2b4ec6b
habznet <habiburrohman900@gmail.com>
2021-09-21 16:44:56 +0700

    mencoba api

---
+++ coba-api.php
@@ -0,0 +1,22 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://localhost:8080/",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+// print_r($response);
+$result  = json_decode($response);
+
+print_r( $result->message);
+curl_close($curl);
+?>
