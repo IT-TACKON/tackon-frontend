44d615c6d77cae84f7bbb30f40f57d799864e08f
habznet <habiburrohman900@gmail.com>
2021-09-23 14:57:34 +0700

    update log folder to sign

--- page/page-search.php
+++ page/page-search.php
@@ -2,8 +2,10 @@
    <h2 class="mt-5">Search</h2>
    <div class="block mt-3">
       <div class="input-group mb-3">
+         <form action="search/" method="POST">
          <input type="text" class="form-control form-control-lg" placeholder="Search post here..." aria-label="Example text with button addon" aria-describedby="button-addon1">
          <button class="btn searchs text-white" type="button" id="button-addon1">ㅤ<i class="fas fa-search fa-lg"></i>ㅤ</button>
+      </form>
       </div>
    </div>
 </div>
