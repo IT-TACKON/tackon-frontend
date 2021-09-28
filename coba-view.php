61f16f0baad2055b6c1cbd36cd6293f2a08f7dca
habznet <habiburrohman900@gmail.com>
2021-09-28 07:40:43 +0700

    bersih bersih

--- coba-view.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$nama = $_SESSION['nama'];
-$nomorhp = $_SESSION['nomorhp'];
-
-echo "namaku ".$nama;
-echo " no hp ku".$nomorhp;
-
-?>
--- coba.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$nama = "Habib";
-$nomorhp = 4567898765456789;
-
-$_SESSION['nama'] = $nama;
-$_SESSION['nomorhp'] = $nomorhp;
-
-?>
